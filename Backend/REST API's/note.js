fdbf7c4fdda02afc44f2ef6d0bf8a55e10e8842b
Punit Sahu <therealpunit5@gmail.com>
2026-02-27 09:47:12 +0530

    Learned about REST API's and its principles

---
+++ Backend/REST API's/note.js
@@ -0,0 +1,9 @@
+// ? Rest Best practices - 
+// * 1. Use JSON representation for req and res.
+// * 2. Never use verbs in endpoint paths - as http methods already have verbs so use noun instead.
+// * 3. Logical Nesting of REST Endpoints - Use nested endpoints when one resource belongs to another, unless you have same database structure.
+// * 4. Handle errors gracefully and return standard error codes
+// * 5. Allow filtering, sorting, and pagination using query params.
+// * 6. Maintain good security practices
+// * 7. Cache data to improve performance
+// * 8. Versioning our APIs
